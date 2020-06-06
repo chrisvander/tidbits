95248a340ee644cdcec6cee2a62ce2bdb7cfce36
Chris Vanderloo <chris.vanderloo@yahoo.com>
2020-06-05 21:10:45 -0400

    Initial Commit

---
+++ tidbits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tidbits
+//
+//  Created by Chris Vanderloo on 6/5/20.
+//  Copyright © 2020 Chris Vanderloo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
